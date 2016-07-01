2789ca804626df322fceba896c165edba2032c9a
Lewis England <lewis.england@caplin.com>
2016-07-01 14:12:08 +0100

    Adding a bit more to the output of the report. Redirecting non-logged in users to the login form

--- web/WebServer.py
+++ web/WebServer.py
@@ -10,21 +10,25 @@ app.jinja_env.globals.update(
     lookup_album=DB.lookupAlbum,
     lookup_albumart=DB.lookupAlbumArt,
     lookup_artist=DB.lookupArtist,
-    lookup_artclass=ImageHandler.setAlbumArtClass
+    lookup_artclass=ImageHandler.setAlbumArtClass,
+    lookup_trackcount=DB.lookupAlbumTrackCount
 )
 
 
 @app.route('/')
 def home():
-    GMusic.load_database()
-    return render_template(
-        'index.html',
-        art_display_size='200',
-        song_count=DB.count('songs'),
-        artist_count=DB.count('artists'),
-        album_count=DB.count('albums'),
-        all_songs=DB.allSongs()
-    ), 200, {'Cache-Control': 'no-cache, no-store', 'Pragma': 'no-cache'}
+    try:
+        GMusic.load_database()
+        return render_template(
+            'index.html',
+            art_display_size='200',
+            song_count=DB.count('songs'),
+            artist_count=DB.count('artists'),
+            album_count=DB.count('albums'),
+            all_songs=DB.allSongs()
+        ), 200, {'Cache-Control': 'no-cache, no-store', 'Pragma': 'no-cache'}
+    except:
+        return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
